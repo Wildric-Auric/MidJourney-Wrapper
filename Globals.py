061a331fa65bf784ef22a34ea2f9a92ad1e7ebeb
Oussama <70033490+Wildric-Auric@users.noreply.github.com>
2022-10-17 19:56:35 +0200

    Update Globals.py
    added forgotten quotes

--- Globals.py
+++ Globals.py
@@ -8,7 +8,6 @@ CHANNEL_ID = [Channel in which commands are sent]
 
 SALAI_TOKEN = [Token of the Account from which you paid MidJourney ]
 
-MID_JOURNEY_ID = 936929561302675456  #midjourney bot id
+MID_JOURNEY_ID = "936929561302675456"  #midjourney bot id
 #don't edit the following variable
-
 targetID       = ""
