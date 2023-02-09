e9bb6be982bb4a72fd65eedc1574406f12d73ef8
mastermind0077 <102423735+mastermind0077@users.noreply.github.com>
2023-02-09 16:23:25 +0530

    Update Globals.py

--- Globals.py
+++ Globals.py
@@ -1,10 +1,10 @@
 #every variable is a string
 
-DAVINCI_TOKEN = [Token of Discord bot]
+DAVINCI_TOKEN = [MTA3Mjk5NzY1MDA4Mjg0MDYwNg.GWwDDi.4ynHVhg9kKcnfgU0EdoRl7Q2-O_Supzb1Jdy6U]
 
-SERVER_ID = [Server id here]
+SERVER_ID = [1066708367063666688]
 
-CHANNEL_ID = [Channel in which commands are sent]
+CHANNEL_ID = [1067149554975264769]
 
 SALAI_TOKEN = [Token of the Account from which you paid MidJourney ]
 
